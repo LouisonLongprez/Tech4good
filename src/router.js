72b66a9020d7ebf898d122f5efee4ae4f175bfbc
LouisonLongprez <146077027+LouisonLongprez@users.noreply.github.com>
2025-09-30 17:57:28 +0200

    ADD router

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  { path: '/', name: 'Home', component: () => import('./components/Home.vue') },
+  { path: '/home', name: 'HomeAlias', component: () => import('./components/Home.vue') },
+  { path: '/about', name: 'About', component: () => import('./components/About.vue') },
+  { path: '/calendrier', name: 'Calendrier', component: () => import('./components/Calendar.vue') }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
