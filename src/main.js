54034b152b21ae5402eb88e3d2b2fdb69967a0a3
LouisonLongprez <louison.longprez@epitech.digital>
2025-09-30 18:30:51 +0200

    [ADD] bonne version

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
+import router from './router'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './components/App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
